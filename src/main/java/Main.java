d1b27fde6d708064cafe7d05d1c5517c858a534f
Dmitrii_Sultanov <Crash1048576@gmail.com>
2017-11-17 14:15:57 +0300

    main method created

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int grid[][] = new int[n][m];
+        for(int grid_i=0; grid_i < n; grid_i++){
+            for(int grid_j=0; grid_j < m; grid_j++){
+                grid[grid_i][grid_j] = in.nextInt();
+            }
+        }
+    }
+}
